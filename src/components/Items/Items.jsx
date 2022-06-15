57276c86b7843a84f94ce80afadc68950c4fe5e4
EveCoronel <evelyncoronel05@gmail.com>
2022-06-15 20:35:06 -0300

    Pequeñas modificaciones

--- src/components/Items/Items.jsx
+++ src/components/Items/Items.jsx
@@ -12,14 +12,14 @@ export default function Items({ producto }) {
   return (
     <>
       <div className="card">
-        <h1 className='h1'>{title}</h1>
+        <h1 className="h1">{title}</h1>
         <span>
           <img className="img" src={pictureUrl} alt={title} />
         </span>
         <p>{`$UY ${price}`}</p>
-        <button className="btn1">{
-        <Link to={`/items/${id}`}>Detalles</Link>
-        }</button>
+        <button className="btn1">
+          {<Link to={`/items/${id}`}>Detalles</Link>}
+        </button>
         {/*  <ItemCount stock={stock} initial={1} onAdd={onAdd} /> */}
         <p>{`Stock disponible: ${stock}`}</p>
       </div>
